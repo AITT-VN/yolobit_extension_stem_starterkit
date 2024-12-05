e6b371a3f961082cf128f442034265d3f6a5353e
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2024-12-05 13:12:11 +0700

    update motor lib to limit speed in -100 to 100

--- stemkit_motor.py
+++ stemkit_motor.py
@@ -72,22 +72,15 @@ class Motor():
 
     def set_wheel_speed(self, m1=None, m2=None):
 
-        if m1 > 100:
-            m1 = 100
-        if m1 < -100:
-            m1 = -100
-        if m2 > 100:
-            m2 = 100
-        if m2 < -100:
-            m2 = -100
-
         # logic to smoothen motion, avoid voltage spike
         # if wheel speed change > 100, need to change to 30 first
         if (m1 != None and m2 != None and m1 != 0 and abs(m1 - self.m1_speed) > 100) and (m2 != 0 and abs(m2 - self.m2_speed) > 100):
+            
             if m1 > 0:
                 # Forward
                 self.ina1.duty(int(translate(30, 0, 100, 0, 1023)))
                 self.ina2.duty(0)
+                
             elif m1 < 0:
                 # Backward
                 self.ina1.duty(0)
@@ -97,6 +90,7 @@ class Motor():
                 # Forward
                 self.inb1.duty(int(translate(30, 0, 100, 0, 1023)))
                 self.inb2.duty(0)
+                
             elif m2 < 0:
                 # Backward
                 self.inb1.duty(0)
@@ -107,10 +101,17 @@ class Motor():
         if m1 != None :
             if m1 >= 0:
                 # Forward
+                if m1 > 100:
+                    m1 = 100
+                    
                 self.ina1.duty(int(translate(abs(m1), 0, 100, 0, 1023)))
                 self.ina2.duty(0)
+                
             elif m1 < 0:
                 # Backward
+                if m1 < -100:
+                    m1 = -100
+                    
                 self.ina1.duty(0)
                 self.ina2.duty(int(translate(abs(m1), 0, 100, 0, 1023)))
             
@@ -119,13 +120,20 @@ class Motor():
         if m2 != None:
             if m2 >= 0:
                 # Forward
+                if m2 > 100:
+                    m2 = 100
+                    
                 self.inb1.duty(int(translate(abs(m2), 0, 100, 0, 1023)))
                 self.inb2.duty(0)
+                
             elif m2 < 0:
                 # Backward
+                if m2 < -100:
+                    m2 = -100
+                    
                 self.inb2.duty(int(translate(abs(m2), 0, 100, 0, 1023)))
                 self.inb1.duty(0)
-            
+
             self.m2_speed = m2
 
     def read_line_sensors(self, index=0):
@@ -154,5 +162,3 @@ motor = Motor()
 
 def stop_all():  # override stop function called by app
     motor.stop()
-
-
